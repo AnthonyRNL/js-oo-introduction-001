157ee471256850b57ed9749f92ab098d48970079
github username <github email address>
2015-07-21 16:37:12 -0400

    bikes and owners

--- js/owner.js
+++ js/owner.js
@@ -1,7 +1,16 @@
-function Owner(){
-  // attributes go here...
+function Owner(name,bikes){
+  this.name = name;
+  this.bikes = [];
 }
 
-Owner.prototype.someInstanceMethod = function(){
-  // code goes here...
-};
+Owner.prototype.buildBike = function(name, bikeType, frameColor){
+  this.bikes.push(new Bike(name, bikeType, frameColor))
+}
+
+Owner.prototype.rideBike = function(nom, miles){
+  this.bikes.forEach(function(e){
+    if(e.name === nom){
+      e.takeForARide(miles)
+    }
+  })
+}
